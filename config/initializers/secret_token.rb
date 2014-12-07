278e5845d9b6c905e17d093f7e3439b0f66c24e9
Luis <luismenjivr@gmail.com>
2014-12-07 10:54:02 -0500

    added_figaro

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Todolist::Application.config.secret_key_base = '0a77be5591912b4bc109b92412bb251e8fed0019be87fd2a8ac5cc1cae267204081f789bda3467334f591e726ffe73e5cce655b7c3bfeb54b809ec1e07c4a82a'
+Todolist::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
