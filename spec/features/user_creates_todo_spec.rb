bea13d9ac7d5117b34ac6b041fcac79dfecbaefa
Luis <luismenjivr@gmail.com>
2014-12-12 12:29:14 -0500

    adding_mail_gun

--- spec/features/user_creates_todo_spec.rb
+++ spec/features/user_creates_todo_spec.rb
@@ -1,8 +1,23 @@
 require 'rails_helper'
   feature 'user creates todo' do 
 
-    scenario 'succesfully'do 
+    scenario 'succesfully' do
+      # Create Users
+      user = User.new(
+        email:    'example@example.com',
+        password: 'password'
+      )
+      
+      user.save!
+
+      visit new_user_session_path
+      fill_in 'Email', with: 'example@example.com'
+      fill_in 'Password', with: 'password'
+      puts page.body
+      click_button 'Log in'
       visit new_todo_path
+
+
       fill_in 'Description', with: 'pay cable bill'
       click_button 'save'
       expect(page).to have_content('Item was saved')
