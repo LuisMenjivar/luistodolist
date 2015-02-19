c9b5198f0f1c4e0981ab22b342ddb101d99aa984
luismenjivar <luismenjivr@gmail.com>
2015-02-18 21:54:43 -0500

    added figaro gem and standard user see and todos

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# Create an standard user
+standard = User.create(
+  email:    'standard@example.com',
+  password: 'helloworld'
+  )
+standard.skip_confirmation!
+standard.save!
+
+5.times do
+  todo = Todo.new(
+    description:   Faker::Hacker.say_something_smart,
+    created_at: Time.now
+    user: standard
+  )
+  todo.save
+end
+todos = Todo.all.count
+ 
+puts "#{todos} todos were created"
